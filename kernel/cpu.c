23ec70c74d4b121a72df6f6c626c8b3250d90147
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-13 17:11:49 +0100

    cpumask: Add cpumask for not-isolated CPUs
    To achieve a similar usage of cpumask functions for hotplug case,
    add a cpumask that contains the mask of not isolated CPUs.
    This allows an easy conversion of pre-existing hotplugging
    algorithms for them to be able to perform CPU isolation without
    major code restructuration.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2242,6 +2242,9 @@ EXPORT_SYMBOL(__cpu_active_mask);
 struct cpumask __cpu_isolated_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_isolated_mask);
 
+struct cpumask __cpu_unisolated_mask __read_mostly;
+EXPORT_SYMBOL(__cpu_unisolated_mask);
+
 void init_cpu_present(const struct cpumask *src)
 {
 	cpumask_copy(&__cpu_present_mask, src);
@@ -2260,6 +2263,11 @@ void init_cpu_online(const struct cpumask *src)
 void init_cpu_isolated(const struct cpumask *src)
 {
 	cpumask_copy(&__cpu_isolated_mask, src);
+}
+
+void init_cpu_unisolated(const struct cpumask *src)
+{
+	cpumask_copy(&__cpu_unisolated_mask, src);
 }
 
 /*
