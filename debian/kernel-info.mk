c30f55d05444df16bacae43b277c1692e00e3dc7
Erik Inkinen <erik.inkinen@gmail.com>
2021-04-04 04:01:30 +0300

    [packaging] Initial packaging

---
+++ debian/kernel-info.mk
@@ -0,0 +1,135 @@
+########################################################################
+# Kernel settings
+########################################################################
+
+# Kernel variant. This is currently used only on the Source package name.
+# Use 'android' for Android kernels ("downstream") or 'mainline' for upstream
+# kernels.
+VARIANT = android
+
+# Kernel base version
+KERNEL_BASE_VERSION = 4.9-0
+
+# The kernel cmdline to use
+KERNEL_BOOTIMAGE_CMDLINE = console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1 firmware_class.path=/vendor/firmware datapart=/dev/mmcblk0p72 cgroup.memory=nokmem lpm_levels.sleep_disabled=1 apparmor=1 security=apparmor
+
+# Slug for the device vendor. This is going to be used in the KERNELRELASE
+# and package names.
+DEVICE_VENDOR = google
+
+# Slug for the device model. Like above.
+DEVICE_MODEL = sargo
+
+# Marketing-friendly full-name. This will be used inside package descriptions
+DEVICE_FULL_NAME = Google Pixel 3a
+
+# Defconfig to use
+KERNEL_DEFCONFIG = lineageos_bonito_defconfig
+
+# Whether to include DTBs with the image. Use 0 (no) or 1.
+KERNEL_IMAGE_WITH_DTB = 1
+
+# Path to the DTB
+# If you leave this undefined, an attempt to find it automatically
+# will be made.
+KERNEL_IMAGE_DTB = arch/arm64/boot/dts/qcom/sdm670.dtb
+
+# Whether to include a DTB Overlay. Use 0 (no) or 1.
+KERNEL_IMAGE_WITH_DTB_OVERLAY = 1
+
+# Path to the DTB overlay.
+# If you leave this undefined, an attempt to find it automatically
+# will be made.
+# KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/google/sdm670-s4-pvt.dtbo
+
+# Whether to include the DTB Overlay into the kernel image
+# Use 0 (no, default) or 1.
+# dtbo.img will always be shipped in the linux-bootimage- package.
+KERNEL_IMAGE_WITH_DTB_OVERLAY_IN_KERNEL = 0
+
+# Path to a specifc configuration file for mkdtboimg.
+# The default is to leave it undefined.
+KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION = arch/arm64/boot/dts/dtboimg-sdm670.cfg
+
+# Path to the DTB directory. Only define if KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION
+# is defined too.
+#KERNEL_IMAGE_DTB_OVERLAY_DTB_DIRECTORY = arch/arm64/boot/dts/google
+
+# Various other settings that will be passed straight to mkbootimg
+KERNEL_BOOTIMAGE_PAGE_SIZE = 4096
+KERNEL_BOOTIMAGE_BASE_OFFSET = 0x00000000
+KERNEL_BOOTIMAGE_KERNEL_OFFSET = 0x00008000
+KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x01000000
+KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0x00f00000
+KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x00000100
+
+########################################################################
+# Automatic flashing on package upgrades
+########################################################################
+
+# Whether to enable kernel upgrades on package upgrades. Use 0 (no) or 1.
+FLASH_ENABLED = 1
+
+# `flash-bootimage` defaults are enough for most recent devices, but legacy
+# devices won't work out of the box.
+# If you set the following to 1, this package will set `flash-bootimage`'s
+# DEVICE_IS_AB and BOOTIMAGE_SLOT_A accordingly, thus enabling flashing
+# on older devices.
+#
+# Do not enable if you don't know what you're doing
+FLASH_IS_LEGACY_DEVICE = 0
+
+# Device manufacturer. This must match the `ro.product.vendor.manufacturer`
+# Android property. If you don't want to specify this, leave it undefined,
+# FLASH_INFO_CPU will be checked instead.
+FLASH_INFO_MANUFACTURER = Google
+
+# Device model. This must match the `ro.product.vendor.model`
+# Android property. If you don't want to specify this, leave it undefined,
+# FLASH_INFO_CPU will be checked instead.
+FLASH_INFO_MODEL = Pixel 3a
+
+# Device CPU. This will be grepped against /proc/cpuinfo to check if
+# we're running on the specific device. Note this is a last-resort
+# method, specifying FLASH_INFO_MANUFACTURER and FLASH_INFO_MODEL is
+# recommended.
+FLASH_INFO_CPU = Qualcomm Technologies, Inc SDM670
+
+########################################################################
+# Kernel build settings
+########################################################################
+
+# Whether to cross-build. Use 0 (no) or 1.
+BUILD_CROSS = 1
+
+# (Cross-build only) The build triplet to use. You'll probably want to
+# use aarch64-linux-android- if building Android kernels.
+BUILD_TRIPLET = aarch64-linux-android-
+
+# (Cross-build only) The build triplet to use with clang. You'll probably
+# want to use aarch64-linux-gnu- here.
+BUILD_CLANG_TRIPLET = aarch64-linux-gnu-
+
+# The compiler to use. Recent Android kernels are built with clang.
+BUILD_CC = aarch64-linux-android-gcc
+
+# Extra paths to prepend to the PATH variable. You'll probably want
+# to specify the clang path here (the default).
+BUILD_PATH = /usr/lib/llvm-android-6.0-4691093/bin
+
+# Extra packages to add to the Build-Depends section. Mainline builds
+# can have this section empty, unless cross-building.
+# The default is enough to install the Android toolchain, including clang.
+DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, clang-android-6.0-4691093, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross, device-tree-compiler, lz4
+
+# Where we're building on
+DEB_BUILD_ON = amd64
+
+# Where we're going to run this kernel on
+DEB_BUILD_FOR = arm64
+
+# Target kernel architecture
+KERNEL_ARCH = arm64
+
+# Kernel target to build
+KERNEL_BUILD_TARGET = Image.lz4-dtb
