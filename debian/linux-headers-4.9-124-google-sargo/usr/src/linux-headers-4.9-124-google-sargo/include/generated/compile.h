5e00e85494695e61dc2fcd7670021552f050ce42
Bardia Moshiri <fakeshell@bardia.tech>
2023-03-19 00:54:22 -0400

    check against  FLASH_INFO_CPU

--- debian/linux-headers-4.9-124-google-sargo/usr/src/linux-headers-4.9-124-google-sargo/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 19 03:37:34 UTC 2023"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "c1d0ffe62151"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
